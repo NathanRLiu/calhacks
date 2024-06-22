2982b2df8b56a4ca7cb5a8ce59507e23ba7fc8e2
cdevadhar <cdevadhar@gmail.com>
2024-06-22 15:23:18 -0700

    changed fetch

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,14 @@ var multipartMiddleware = multipart();
 const app = express();
 
 app.post("/upload", multipartMiddleware, (req, res) => {
-    fetch("https://server.simpletex.cn/api/latex_ocr", req.files, {headers: {"token": "yJAfYlAvgWbtaw1MstE1c98neKLFZKUAmBRWB6RiWzNI6dpjheLhloOEESUyqv64"}})
+    fetch("https://server.simpletex.cn/api/latex_ocr", {
+        method: "POST",
+        headers: {
+            "token": "yJAfYlAvgWbtaw1MstE1c98neKLFZKUAmBRWB6RiWzNI6dpjheLhloOEESUyqv64"
+        },
+        body: req.files
+    })
+    .then((res) => console.log(res))
     .catch((err) => {console.log(err)});
 });
 
