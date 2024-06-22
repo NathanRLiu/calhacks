7cec81b77bc01ed1bbff30c94657f68a464473b3
stewartkwokca <stewart.kwok1@gmail.com>
2024-06-22 14:35:39 -0700

    add some file uploading

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const axios = require("axios");
+
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+app.post("/upload", multipartMiddleware, (req, res) => {
+    axios.post("https://server.simpletex.cn/api/latex_ocr", req.files, {headers: {"token": "yJAfYlAvgWbtaw1MstE1c98neKLFZKUAmBRWB6RiWzNI6dpjheLhloOEESUyqv64"}})
+    .catch((err) => {console.log(err)});
+});
 
 app.listen(8000);
