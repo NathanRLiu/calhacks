b89f2775822c16cd8083414ef25f79c7273b62f7
cdevadhar <cdevadhar@gmail.com>
2024-06-22 15:47:01 -0700

    multer

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,38 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const axios = require("axios");
-
-var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart();
+const fs = require('fs');
 
 const app = express();
 
-app.post("/upload", multipartMiddleware, (req, res) => {
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+  
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    const filePath = path.join(__dirname, 'uploads', req.file.filename);
+    // console.log(req.files);
+    const formData = new FormData();
+    formData.append('file', fs.createReadStream(filePath));
     fetch("https://server.simpletex.cn/api/latex_ocr", {
         method: "POST",
         headers: {
             "token": "yJAfYlAvgWbtaw1MstE1c98neKLFZKUAmBRWB6RiWzNI6dpjheLhloOEESUyqv64"
         },
-        body: req.files
+        body: formData
     })
     .then((res) => console.log(res))
     .catch((err) => {console.log(err)});
+    res.send(req.files);
 });
 
 app.listen(8000);
