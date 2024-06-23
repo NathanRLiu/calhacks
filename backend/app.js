8c22fffd49cfc03f62601382e7b5e2b49e35c9ad
cdevadhar <cdevadhar@gmail.com>
2024-06-22 21:01:16 -0700

    made stuff async

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-  
+
 
 app.post("/upload", upload.single("file"), async (req, res) => {
     const filePath = path.join(__dirname, 'uploads', req.file.filename);
@@ -34,17 +34,20 @@ app.post("/upload", upload.single("file"), async (req, res) => {
         const result = await results.json();
         const lines = encodeURIComponent(result.res.latex).split("\\\\");
         let out = [];
+        let promises = [];
         console.log(lines);
         for (let line of lines){
             console.log(line);
-            await fetch(`http://api.wolframalpha.com/v2/query?appid=8KJERX-4X9LWJK2QL&input=${line}&output=json`)
+            const promise = fetch(`http://api.wolframalpha.com/v2/query?appid=8KJERX-4X9LWJK2QL&input=${line}&output=json`)
             .then(async(resp) => {
                 const result = await resp.json();
                 console.log(result);
                 console.log(result.queryresult.pods);
                 out.push(result.queryresult.pods);
-            })
+            });
+            promises.push(promise);
         };
+        await Promise.all(promises);
         return res.send(out);
     })
     .catch((err) => {
