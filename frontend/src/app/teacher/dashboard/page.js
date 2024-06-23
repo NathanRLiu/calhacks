55f13db37801b946612609a67bfb380141a6a337
Nathan Liu <nathanliu17@gmail.com>
2024-06-23 10:51:48 -0700

    no more dark mode

--- frontend/src/app/teacher/dashboard/page.js
+++ frontend/src/app/teacher/dashboard/page.js
@@ -21,8 +21,8 @@ export default function Home() {
         <div className="bg-gray-100 absolute w-5/6 h-2/3 p-6 px-0 rounded-md ">
           <h1 className="text-gray-700 text-left font-semibold ml-6 mb-6 text-black text-3xl">Teacher Dashboard </h1>
             <div class="relative overflow-x-auto sm:rounded-lg">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
                         <tr>
                             <th scope="col" class="px-6 py-4">
                               Assignment
@@ -44,8 +44,8 @@ export default function Home() {
                     <tbody>
                       {
                         (assignments.map((submission, index) => (
-                          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          <tr class="bg-white border-b hover:bg-gray-50 ">
+                              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
                                 {submission.assignment_name}
                               </th>
                               <td class="px-6 py-4">
